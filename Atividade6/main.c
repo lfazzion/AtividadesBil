0ed91155fbd1fe956ae4ddc335d65d83abef41b3
Lowell Fazzion <lowellfazzion@gmail.com>
2024-09-28 12:28:17 -0300

    bug corrigido

--- Atividade6/main.c
+++ Atividade6/main.c
@@ -3,20 +3,20 @@
 int main() {
     int i = 0, j = 0;
     int matriz[2][2] = {};
-    for (; i < 2; i++) {
-        for (; j < 2; j++) {
+
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
             printf("Digite o valor da posicao %d %d: \n", i + 1, j + 1);
             scanf("%d", &matriz[i][j]);
-            printf("i: %d, j: %d\n", i, j);
         }
     }
-    i = 0;
-    j = 0;
-    for (; i < 2; i++) {
-        for (; i < 2; i++) {
+
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
+
     return 0;
 }
